ad16138d9e578f948f705870cacd54dfebc36fe5
htetsoemoe <soemoehtet.mdy@gmail.com>
2023-09-12 09:33:11 +0630

    finished project setup and running

---
+++ food-app/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-semibold">Hello from React Tailwind CSS Food Application</h1>
+    </div>
+  )
+}
+
+export default App
